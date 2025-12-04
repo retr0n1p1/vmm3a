c5bb9811cfa859b2488126297e24ad51d61d893d
Artyom Tishkov <artemtishkov0508@gmail.com>
2025-12-04 13:39:29 +0300

    Cleaning and new readme

--- Release/run.sh
+++ Release/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 rm kuku.bin
 make clean
-make "VMM3A FEB v1.0.elf" -j4
-arm-none-eabi-objcopy -O binary VMM3A\ FEB\ v1.0.elf kuku.bin
+make "VMM3Atrig.elf" -j4
+arm-none-eabi-objcopy -O binary VMM3Atrig.elf kuku.bin
